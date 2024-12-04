da672bf3dc7e220de69f2f343b9827d26bac4635
gabsma25 <gabriela.alem@alunos.uerr.edu.br>
2024-12-04 18:08:24 -0400

    updt: products

--- buyfast/products/views.py
+++ buyfast/products/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
@@ -9,7 +9,17 @@ class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = "products/list.html"
     
-    #def get_context_data(self, *args, **kwargs):
-        #context = super(ProductListView, self).get_context_data(*args, **kwargs)
-        #print(context)
-        #return context
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+class ProductDetailView(DetailView):
+    #traz todos os produtos do banco de dados sem filtrar nada 
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
