bd52c123cc58e85848054a4dee9d245458deb210
gabsma25 <gabriela.alem@alunos.uerr.edu.br>
2024-12-04 05:00:45 -0400

    feat: emulador de cdn comoambiente estatico

--- buyfast/src/settings.py
+++ buyfast/src/settings.py
@@ -98,7 +98,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_local")]
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
