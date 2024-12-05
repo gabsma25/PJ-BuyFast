e808f77d779693dfb7c0824a2a37d265b247e456
gabsma25 <gabriela.alem@alunos.uerr.edu.br>
2024-12-05 08:28:17 -0400

    feat: link de logout funcional

--- buyfast/src/urls.py
+++ buyfast/src/urls.py
@@ -4,13 +4,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import (home_page, about_page, contact_page, login_page, register_page)
+from .views import (home_page, about_page, contact_page, login_page, register_page, logout_page,)
 
 urlpatterns = [
     path('', home_page, name = 'home'),
     path('about/', about_page, name = 'about'),
     path('contact/', contact_page, name = "contact"),
     path('login/', login_page, name = 'login'),
+    path('logout/', logout_page, name='logout'),
     path('register/', register_page, name ='register' ),
     path('products/', include("products.urls", namespace="products")),
     path('admin/', admin.site.urls),
