c3610a151849d478c9ad11629d98b5179ff85a9b
gabsma25 <gabriela.alem@alunos.uerr.edu.br>
2024-12-05 08:14:47 -0400

    fix: concertar reponsibidade de produtos no navbar

--- buyfast/products/urls.py
+++ buyfast/products/urls.py
@@ -7,6 +7,6 @@ from .views import (
                         ProductDetailSlugView,
                     )
 urlpatterns = [
-    path('', ProductListView.as_view()),
+    path('', ProductListView.as_view(), name= 'list'),
     path('<slug:slug>/', ProductDetailSlugView.as_view(), name='detail')
     ]
